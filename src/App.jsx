be01ba1879a8f23b269337a7af3b96bf91740d5a
David-git-klo <davide319339@gmail.com>
2025-06-30 21:23:48 +0630

    click button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
 import { useState } from 'react';
-import Layout from './components/Layout'
+import Layout from './components/Layout';
 
 function App() {
-  let [posts, setPosts] = useState([
+  const [posts, setPosts] = useState([
     {
       id: 1,
       title: "First Post",
@@ -11,20 +10,35 @@ function App() {
       author: "Dave"
     },
     {
-      id: 1,
+      id: 2, // Changed ID to 2 for the second post to be unique
       title: "Second Post",
       body: "This is Second post",
       author: "John"
+    },
+    {
+      id: 3, // Added a post with ID 3
+      title: "Third Post",
+      body: "This is the amazing third post!",
+      author: "Alice"
     }
   ]);
+
+  // Function to find and set the post with ID 3
+  const showPostWithId3 = () => {
+    const postWithId3 = posts.find(post => post.id === 3);
+    if (postWithId3) {
+      setPosts([postWithId3]); // Set the posts array to only contain the post with ID 3
+    }
+  };
+
   return (
     <Layout>
-{posts.map((post) => {
-  return <h1 key={post.id}>{post.title}</h1>
-})}
-{/* <button onClick={() => setPosts([post])}>Click </button> */}
+      {posts.map((post) => {
+        return <h1 key={post.id}>{post.title}</h1>
+      })}
+      <button onClick={showPostWithId3}>Show Post with ID 3</button>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
